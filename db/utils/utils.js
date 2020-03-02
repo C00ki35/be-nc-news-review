53c035dd9a44c5eea779e62d4e2dc8bb696bc7cd
Cooki3 <pgcdigit@gmail.com>
2020-03-02 15:16:37 +0000

    format dates / makeRefObj functions completed with testing

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,18 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const changedlist = list.map(item => {
+    const itemCopy = { ...item };
+    itemCopy.created_at = new Date(itemCopy.created_at);
+    return itemCopy;
+  });
+  return changedlist;
+};
 
-exports.makeRefObj = list => {};
-
-exports.formatComments = (comments, articleRef) => {};
+exports.makeRefObj = list => {
+  if (list.length === 0) return {};
+  const refObj = {};
+  list.forEach(item => {
+    const copyItem = { ...item };
+    refObj[copyItem.title] = copyItem.article_id;
+  });
+  return refObj;
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,144 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  it("Returns empty array if given an empty array of objects", () => {
+    expect(formatDates([])).to.eql([]);
+  });
+  it("Returns correct datestamp for one object in array", () => {
+    expect(
+      formatDates([
+        {
+          title: "Running a Node App",
+          topic: "coding",
+          author: "jessjelly",
+          body:
+            "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+          created_at: 1471522072389
+        }
+      ])
+    ).to.eql([
+      {
+        title: "Running a Node App",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+        created_at: new Date(1471522072389)
+      }
+    ]);
+  });
+  it("Returns correct datestamp for MORE THAN ONE object in an array", () => {
+    expect(
+      formatDates([
+        {
+          title: "Running a Node App",
+          topic: "coding",
+          author: "jessjelly",
+          body:
+            "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+          created_at: 1471522072389
+        },
+        {
+          title:
+            "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+          topic: "coding",
+          author: "jessjelly",
+          body:
+            "Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
+          created_at: 1500584273256
+        }
+      ])
+    ).to.eql([
+      {
+        title: "Running a Node App",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+        created_at: new Date(1471522072389)
+      },
+      {
+        title:
+          "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
+        created_at: new Date(1500584273256)
+      }
+    ]);
+  });
+  it("Does NOT MUTATE the original array", () => {
+    const originalArray = [
+      {
+        title: "Running a Node App",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+        created_at: 1471522072389
+      },
+      {
+        title:
+          "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
+        created_at: 1500584273256
+      }
+    ];
+    formatDates(originalArray);
+    expect(originalArray).to.eql([
+      {
+        title: "Running a Node App",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+        created_at: 1471522072389
+      },
+      {
+        title:
+          "The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+        topic: "coding",
+        author: "jessjelly",
+        body:
+          "Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
+        created_at: 1500584273256
+      }
+    ]);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {
+  it("Will return a empty object if given empty array", () => {
+    makeRefArray = [];
+    expect(makeRefObj(makeRefArray)).eql({});
+  });
 
-describe('formatComments', () => {});
+  it("Will return ONE KEY/VALUE pair if given on array with ONE OBJECT", () => {
+    makeRefArray = [{ article_id: 1, title: "A" }];
+    expect(makeRefObj(makeRefArray)).eql({ A: 1 });
+  });
+
+  it("Will return MULTIPLE KEY/VALUE pairs if given on array with MORE THAN ONE OBJECT", () => {
+    makeRefArray = [
+      { article_id: 1, title: "A" },
+      { article_id: 2, title: "B" },
+      { article_id: 3, title: "C" }
+    ];
+    expect(makeRefObj(makeRefArray)).eql({ A: 1, B: 2, C: 3 });
+  });
+
+  it("Does NOT MUTATE original object", () => {
+    makeRefArray = [{ article_id: 1, title: "A" }];
+    makeRefObj(makeRefArray);
+    expect(makeRefArray).to.deep.equal([{ article_id: 1, title: "A" }]);
+  });
+});
