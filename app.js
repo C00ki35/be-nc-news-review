36080cc879c42a47b270e9e84d81c3d9ad25b93f
Cooki3 <pgcdigit@gmail.com>
2020-03-23 12:06:55 +0000

    cors added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const { psqlErrors, handle500, handle404s } = require("./errors");
 const apiRouter = require("./routes/apiRouter");
 const { error405 } = require("./errors");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
